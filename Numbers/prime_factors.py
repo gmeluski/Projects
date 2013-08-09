ea151d45e0142db9ac65611d84d273be1b7c2fa4
Gregg Meluski <gmeluski@hotmail.com>
2013-08-09 11:53:40 -0400

    finishing up prime factors

--- Numbers/prime_factors.py
+++ Numbers/prime_factors.py
@@ -9,31 +9,18 @@ def isPrime(numberToCheck):
 
 def findPrimeFactors(numberToCheck):
     if (isPrime(numberToCheck)):
-        print numberToCheck
+        print numberToCheck,
         return True
     else:
-        print 'range'
         for divisor in range(2, numberToCheck):
             if (numberToCheck % divisor == 0):
                 # find the other number
-                print divisor
-                print isPrime(divisor)
-                foundFactor = numberToCheck / divisor
-                print foundFactor
-                print isPrime(foundFactor)
+                print divisor,
+                if (isPrime(divisor)):
+                    foundFactor = numberToCheck / divisor
+                    findPrimeFactors(foundFactor) 
                 return
-
-# if the original number is prime, just return the original number
-# otherwise start a counter / range at 2
-# if this number divides evenly
-#   log the original counter
-#   find the other number.
-#   if that number is prime
-#       log that number
-# while the modulo is 0
-#   get the other number
-
 
 userMessage = 'Enter a number to find all prime factors:'
 originalNumber = int(helpers.getUserInput(userMessage))
-print findPrimeFactors(originalNumber)
+findPrimeFactors(originalNumber)
