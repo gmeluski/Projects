934b7f31495e6b19cac59bed8be79867454f0c6c
Gregg Meluski <gmeluski@hotmail.com>
2013-08-12 07:32:59 -0400

    finishing up prime numbers

--- Numbers/find_prime.py
+++ Numbers/find_prime.py
@@ -1,22 +1,11 @@
 import helpers
 
-# generate primeFromPoint(startPoint)
-#    primeTest = isPrime(startPoint)
-#    if primeTest
-#       return startPoint
-#   else
-#       while (!primeTest)
-#           startPoint equals startPoint + 1
-#           if startPoint mod 2 does not equal 0
-#               primeTest = isPrime(startPoint)
-#        
-
 def generatePrimeFromPoint(startPoint):
     primeTest = helpers.isPrime(startPoint)
     while (not primeTest):
-        startPoint = startPoint + 1
+        startPoint += 1
         if (startPoint % 2 != 0):
-            primeTest = isPrime(startPoint)   
+            primeTest = helpers.isPrime(startPoint)
     return startPoint
 
 def primeMessage(numberForMessage):
@@ -28,11 +17,7 @@ def primeMessage(numberForMessage):
 
 startPoint = 2
 userResult = primeMessage(startPoint)
-if (userResult):
-    generatePrimeFromPoint(startPoint)
-
-# print lastPrime, then a prompt to continue
-# if continue
-#   generate a prime number based on the last number
-# else 
-#   print goodbye!
+while (userResult):
+    startPoint += 1
+    startPoint = generatePrimeFromPoint(startPoint)
+    userResult = primeMessage(startPoint)
