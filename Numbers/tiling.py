7a47da41620f1c8f2980741effb112698ecc4037
Gregg Meluski <gmeluski@hotmail.com>
2013-08-13 08:00:59 -0400

    adding in square footage problem

---
+++ Numbers/tiling.py
@@ -0,0 +1,12 @@
+width = int(raw_input('Enter a width: '))
+height = int(raw_input('Enter a height: '))
+cost = float(raw_input('Enter a cost: '))
+
+print "Total cost $%.2f" % (width*height*cost)
+
+
+
+
+
+
+
