8fb073ad4191a046659e5b171c33a9cd94bfc879
Gregg Meluski <gmeluski@hotmail.com>
2013-08-25 09:32:41 -0400

    starting work on the conversion program

---
+++ Numbers/converter.py
@@ -0,0 +1,19 @@
+conversionPrompts = {
+    't': 'Enter a temperature in Celsius',
+    'c': 'Enter an amount in dollars',
+    'm': 'Enter a weight in pounds'
+}
+
+def convertTemperature(temperature):
+    print
+
+def f(x):
+    
+    originalNumber = raw_input(conversionPrompts[conversionDirection])
+    return {
+        'a': convertTemperature(),
+        'b': 2,
+    }[x]
+
+conversionDirection = raw_input('Convert by temperature, currency or mass (t / c / m): ')
+f(conversionPoint)
