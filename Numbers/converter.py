d2ab3393a1831c714462f461357cdbc1b80e098b
Gregg Meluski <gmeluski@hotmail.com>
2013-09-02 12:11:40 -0400

    cleaning up converter

--- Numbers/converter.py
+++ Numbers/converter.py
@@ -1,19 +1,39 @@
-conversionPrompts = {
-    't': 'Enter a temperature in Celsius',
-    'c': 'Enter an amount in dollars',
-    'm': 'Enter a weight in pounds'
-}
+class Conversions: 
+    conversionPrompts = {
+            't': {
+                'prompt': 'Enter a temperature in Farenheit: ',
+                'function': temperatureFormula
+            },  
+            'c': {
+                'prompt': 'Enter an amount in dollars: ',
+                'function': currencyFormula 
+            },
+            'm': {
+                'prompt': 'Enter a weight in pounds: ',
+                'function': massFormula    
+            }
+    }
+    def temperatureFormula(self, farenheit):
+        return (farenheit - 32) * (5./9.)
 
-def convertTemperature(temperature):
-    print
+    def currencyFormula(self, dollars):
+        return dollars * 0.75 
 
-def f(x):
-    
-    originalNumber = raw_input(conversionPrompts[conversionDirection])
-    return {
-        'a': convertTemperature(),
-        'b': 2,
-    }[x]
+    def massFormula(self, pounds):
+        dataPoint = returnConversionPrompt('m')
+        return pounds / 2.2
+
+
+    def returnConversionPrompt(self, conversionPrompt):
+        return float(raw_input(conversionPrompt))
+
+
+    def setConversionFromInput(self, userSelection):
+        lookupResult = self.conversionPrompts[userSelection]
+        userInput = self.returnConversionPrompt(lookupResult['prompt'])
+        computedValue = lookupResult['function'](self, userInput)  
+        print computedValue
 
 conversionDirection = raw_input('Convert by temperature, currency or mass (t / c / m): ')
-f(conversionPoint)
+conversions = Conversions()
+conversions.setConversionFromInput(conversionDirection)
